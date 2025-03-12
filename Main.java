4f5afe4ad91871667e7dd99d90dd5bb6f0714df7
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-03-12 11:22:34 +0530

    Created variables for MyThread and Created Constructor

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 // Main.java
 
-class MyThread implements Runnable{
+class MyThread extends Thread{
+	
+	private int init;
+	private int increment;
+	private int delay;
+	
+	public MyThread(int init, int increment, int delay){
+		this.init = init;
+		this.increment = increment;
+		this.delay = delay;
+	}
+	
 	public void run(){
-		System.out.println(" Thread Started ");
+		
 	}
 }
 
